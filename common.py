f990f9f6c644deed76fefdcd22728263275e51aa
Marcell Lovácsi <m.lovacsi@gmail.com>
2016-11-23 10:29:19 +0100

    random generation of ID's added it also checks if an ID is already in the given table and regenerates the current ID to try and make it different

--- common.py
+++ common.py
@@ -1,7 +1,7 @@
 # implement commonly used functions here
 
 import random
-
+# A -Z 65-90  a -z 97 122 spec 33-38
 
 # generate and return a unique and random string
 # other expectation:
@@ -10,10 +10,39 @@ import random
 #
 # @table: list of list
 # @generated: string - generated random string (unique in the @table)
+
+
 def generate_random(table):
+    flag = 1
+    while flag == 1:
+        generated = ('' + gen_random_lower() + gen_random_upper() + gen_random_number() + gen_random_number())
+        generated = (generated + gen_random_upper() + gen_random_lower() + gen_random_spec() + gen_random_spec())
+        for i in table:
+            if generated in i:
+                flag = 1
+            if generated not in i:
+                flag = 0
+        return generated
 
-    generated = ''
 
-    # your code
+def gen_random_number():
+    gen_random_number = random.randint(0, 9)
+    return str(gen_random_number)
 
-    return generated
+
+def gen_random_lower():
+    gen_random_lower_utf = random.randint(97, 123)
+    gen_random_lower = chr(gen_random_lower_utf)
+    return gen_random_lower
+
+
+def gen_random_upper():
+    gen_random_upper_utf = random.randint(65, 91)
+    gen_random_upper = chr(gen_random_upper_utf)
+    return gen_random_upper
+
+
+def gen_random_spec():
+    gen_random_spec_utf = random.randint(33, 39)
+    gen_random_spec = chr(gen_random_spec_utf)
+    return gen_random_spec
