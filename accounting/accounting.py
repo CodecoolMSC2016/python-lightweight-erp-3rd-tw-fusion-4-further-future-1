d63f451dba08576b5bb11cf37bca7adce6168d04
bodoralex <bodaleaa@gmail.com>
2016-11-24 11:49:57 +0100

    accounting basic working

--- accounting/accounting.py
+++ accounting/accounting.py
@@ -39,11 +39,20 @@ def start_module():
     elif decide == "2":
         current_table = data_manager.get_table_from_file(
             'accounting/items.csv')
-        add(current_table)
+        data_manager.write_table_to_file(
+            'accounting/items.csv', add(current_table))
     elif decide == "3":
-        remove()
+        current_table = data_manager.get_table_from_file(
+            'accounting/items.csv')
+        identificator = ui.get_inputs("Enter an ID to delete", "")
+        data_manager.write_table_to_file(
+            'accounting/items.csv', remove(current_table, identificator))
     elif decide == "4":
-        update()
+        current_table = data_manager.get_table_from_file(
+            'accounting/items.csv')
+        identificator = ui.get_inputs("Enter an ID to update", "")
+        data_manager.write_table_to_file(
+            'accounting/items.csv', update(current_table, identificator))
     elif decide == "0":
         pass
 
@@ -52,11 +61,8 @@ def start_module():
 #
 # @table: list of lists
 def show_table(table):
-    title_list = ["id", "month", "day", "year", "type", "amount"]
+    title_list = ["month", "year", "day", "year", "type", "amount"]
     ui.print_table(table, title_list)
-    # your code
-
-    pass
 
 
 # Ask a new record as an input from the user than add it to @table, than return @table
@@ -64,8 +70,12 @@ def show_table(table):
 # @table: list of lists
 def add(table):
 
-    # your code
-
+    title_list = ["month", "day", "year", "type: in or out(come)", "amount"]
+    args = []
+    args.append(common.generate_random(table))
+    for arg in range(len(title_list)):
+        args.append(ui.get_inputs(("Please enter the " + title_list[arg]), ""))
+    table.append(args)
     return table
 
 
@@ -75,9 +85,10 @@ def add(table):
 # @id_: string
 def remove(table, id_):
 
-    # your code
-
-    return table
+    for row in range(len(table)):
+        if table[row][0] == id_:
+            table.remove(table[row])
+            return table
 
 
 # Update the record in @table having the id @id_ by asking the new data from the user,
@@ -87,9 +98,16 @@ def remove(table, id_):
 # @id_: string
 def update(table, id_):
 
-    # your code
+    title_list = ["month", "day", "year", "type: in or out(come)", "amount"]
+    args = []
+    args.append(id_)
+    for arg in range(len(title_list)):
+        args.append(ui.get_inputs(("Please enter the " + title_list[arg]), ""))
 
-    return table
+    for row in range(len(table)):
+        if table[row][0] == id_:
+            table[row] = args
+            return table
 
 
 # special functions:
